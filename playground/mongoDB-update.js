6b64c06f653f6e00e901896f44f1a25826b55be0
Nils Koliha <nils.koliha@gmail.com>
2018-09-07 17:19:29 -0700

    Add update script

---
+++ playground/mongoDB-update.js
@@ -0,0 +1,41 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true}, (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  var db = client.db('TodoApp');
+
+  // findOneAndUpdate
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5b930ffb4aeb641c1cb9b7ff')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5b9304570ae5ad7b1d4e54ff')
+  }, {
+    $set: {
+      name: 'Nils'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+
+  // client.close();
+});
